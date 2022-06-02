a5b29314a047a3ada6b8cdbce0fe5de284eb12b0
jmiarons <joelmiarons@gmail.com>
2022-06-02 17:45:35 +0200

    Added speed plot

--- f1-data.py
+++ f1-data.py
@@ -16,7 +16,20 @@ def total_laps_plot():
         ax.plot(i['LapNumber'], i['LapTime'], label=driver_list[j])
         j += 1
     ax.set(ylabel='Time', xlabel='Laps')
-    ax.legend(loc="upper center")
+    ax.legend(loc='upper center')
+
+    plt.show()
+
+def speed_plot():
+    plt.rcParams['figure.figsize'] = [10, 6]
+
+    fig, ax = plt.subplots()
+    j = 0
+    for i in speed_drivers_car:
+        ax.plot(i['Time'], i['Speed'], label=driver_list[j])
+        j += 1
+    ax.set(xlabel='Time', ylabel='Speed [Km/h]')
+    ax.legend(loc='upper center')
 
     plt.show()
 
@@ -30,9 +43,11 @@ ff1.Cache.enable_cache('cache')
 
 year=2022
 circuit='Monaco'
-session='R'
+session='Q'
 
 drivers = []
+speed_drivers = []
+speed_drivers_car = []
 
 # Data from race and quali
 race = ff1.get_session(year, circuit, session)
@@ -43,8 +58,11 @@ driver_list= ['NOR', 'RUS', 'ALO']
 
 for i in driver_list:
     drivers.append(race.laps.pick_driver(i))
+    speed_drivers.append(race.laps.pick_driver(i).pick_fastest())
+    speed_drivers_car.append(race.laps.pick_driver(i).pick_fastest().get_car_data())
 
 # Comparison between two drivers the whole race
-total_laps_plot()
+# total_laps_plot()
+speed_plot()
 
 
